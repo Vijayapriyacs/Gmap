5b5a87180de7ec4df66123d026665090615153b8
vijayapriyapalanikumar <vijayapriya100@gmail.com>
2018-07-04 13:23:59 +0530

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,11 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.gMap = new GMap.NET.WindowsForms.GMapControl();
             this.label1 = new System.Windows.Forms.Label();
             this.CreateRoute = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // splitter1
@@ -55,6 +57,7 @@
             this.gMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMap.LevelsKeepInMemmory = 5;
             this.gMap.Location = new System.Drawing.Point(0, 0);
+            this.gMap.Margin = new System.Windows.Forms.Padding(0);
             this.gMap.MarkersEnabled = true;
             this.gMap.MaxZoom = 100;
             this.gMap.MinZoom = 5;
@@ -88,9 +91,9 @@
             // 
             // CreateRoute
             // 
-            this.CreateRoute.Location = new System.Drawing.Point(876, 233);
+            this.CreateRoute.Location = new System.Drawing.Point(863, 95);
             this.CreateRoute.Name = "CreateRoute";
-            this.CreateRoute.Size = new System.Drawing.Size(158, 23);
+            this.CreateRoute.Size = new System.Drawing.Size(85, 23);
             this.CreateRoute.TabIndex = 9;
             this.CreateRoute.Text = "Create Route";
             this.CreateRoute.UseVisualStyleBackColor = true;
@@ -98,7 +101,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(886, 279);
+            this.button1.Location = new System.Drawing.Point(974, 95);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 10;
@@ -107,18 +110,30 @@
             this.button1.Click += new System.EventHandler(this.gMapControl1_Load);
             this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.AskFileName);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(863, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1061, 686);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.CreateRoute);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.gMap);
             this.Controls.Add(this.splitter1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SpectrumEmbedded Solution Route Download";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -132,6 +147,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button CreateRoute;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
