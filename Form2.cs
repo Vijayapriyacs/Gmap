88a630ade89e8c506213c1683c0b360321aedd7b
vijayapriyapalanikumar <vijayapriya100@gmail.com>
2018-07-04 12:27:23 +0530

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusRoutePath
+{
+    public partial class Form2 : Form
+    {
+        public string Result { get; set; }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Result = textBox1.Text;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Result = textBox1.Text;
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
